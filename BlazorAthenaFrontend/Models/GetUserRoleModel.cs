279aa781a7c4473b935c9faab663b36f101077db
joelblad1 <joelblad@hotmail.se>
2023-12-20 03:38:00 +0100

    Working

---
+++ BlazorAthenaFrontend/Models/GetUserRoleModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Permissions;
+
+namespace BlazorAthenaFrontend.Models
+{
+    public class GetUserRoleModel
+    {
+        public string email { get; set; }
+        public string role { get; set; }
+        public string userId { get; set; }
+    }
+}
